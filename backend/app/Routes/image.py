6fe351f847fc81c3fd58e484fe03634414532dec
xShiro2 <daryllejumalon16@gmail.com>
2023-02-16 08:47:55 +0800

    fix: return 404 if img not found

--- backend/app/Routes/image.py
+++ backend/app/Routes/image.py
@@ -1,11 +1,18 @@
 from flask import request, send_file
 from app import app
 from app.Components.image_handler import get_img
+from app.Components.response import Response
 
 @app.route('/api/v1/images/<image>', methods=['GET'])
 def image(image):
     if request.method == 'GET':
         img = get_img(image)
+
+        if not img:
+            return Response(
+                status=404,
+            )
+        
         return send_file(
             img,
             mimetype='image/png'
